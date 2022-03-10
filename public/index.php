37da273e48dca5e9adc92da6dd6bb64cbc8a4188
Anewh <i0ianrusi0i@gmail.com>
2022-03-10 14:47:29 +0300

    little changes

--- public/index.php
+++ public/index.php
@@ -1,47 +1,41 @@
-<?php session_start(); ?>
-<!doctype html>
-<html>
-	<head>
-		<title>Лаба №2</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/style.css"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
 
-		<?php if (!isset($_SESSION['user_name'])): ?>
- 			<script type="text/javascript" src="js/popups.js" defer></script>
+require_once '../vendor/autoload.php';
 
- 		<?php else: ?>
-  			<script type="text/javascript" src="js/after_login.js" defer></script>
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
- 		<?php endif; ?>
+$request = Request::createFromGlobals();
+$session = new Session();
+$session->start();
+$request->setSession($session);
 
-		<script type="module" src="js/pages_loading.js" defer></script>
-		<script type="module" src="js/previews_loading.js" defer></script>
-    	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-	</head>
-	<body>
-	<div class="wrapper">
+$fileLocator = new FileLocator([__DIR__ . '/../config']);
+$loader = new YamlFileLoader($fileLocator);
+$routes = $loader->load('routes.yaml');
 
-		<?php //require_once 'fragments\header.php'; ?>
-		
+$context = new RequestContext('/');
 
-		<div class="page-title">
-			<h1>Find right movie for evening </h1>
-			<div class="page-text"><b>Discuss new or favorite movies with other moviegoers ;)<br>Send good films for your friend</b></div>
-			<button class=" btn page-title__btn" type="button">Start searching</button>
-		</div>
-		<main class="main">
-			<div id="cardsBlock" class="card-block" data-lastid="0">
-				<!-- ajax  -->
-			</div>
-			<div class="loading-gif__wrapper" id="loadingGifWrapper">
-					<img class="loading-gif" src="./resources/loader.gif" alt="Раньше здесь был мат">
-			</div>
-		</main>
+$matcher = new UrlMatcher($routes, $context);
+try {
+    $route = $matcher->matchRequest($request);
+    list($controllerName, $actionName) = explode("::", $route['_controller']);
 
-		<?php //require_once 'fragments\footer.php'; ?>
+    $controller = new $controllerName($request);
+    $response = $controller->$actionName($route);
 
-	</div>
-	</body>
-</html>
+    if (!$response instanceof Response) {
+        $response = new Response('Ошибка: неверный ответ от контролера', 500);
+    }
+} catch (ResourceNotFoundException $exception) {
+    $response = new Response('Страница не найдена', 404);
+} catch (Exception $exception) {
+    $response = new Response('Ошибка: ' . $exception->getMessage(), 500);
+}
+$response->send();
